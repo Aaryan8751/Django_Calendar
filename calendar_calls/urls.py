f1f79a92cba543586e2d5ff16ea5386e664e5e26
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2023-03-01 15:22:50 +0530

    google calendar redirect view done

--- calendar_calls/urls.py
+++ calendar_calls/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('rest/v1/calendar/init/',views.GoogleCalendarInitView,name='google_init'),
-    # path('/rest/v1/calendar/redirect/',views.GoogleCalendarRedirectView,name='google_redirect_view')
+    path('rest/v1/calendar/redirect/',views.GoogleCalendarRedirectView,name='google_redirect_view')
 ]
--- calendar_calls/views.py
+++ calendar_calls/views.py
@@ -4,7 +4,13 @@ from rest_framework.response import Response
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
+import os
 # Create your views here.
+
+# When running locally, disable OAuthlib's HTTPs verification.
+  # ACTION ITEM for developers:
+  #     When running in production *do not* leave this option enabled.
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 SCOPES = ['https://www.googleapis.com/auth/calendar',
 'https://www.googleapis.com/auth/calendar.readonly',
@@ -32,6 +38,67 @@ def GoogleCalendarInitView(request):
   # Store the state so the callback can verify the auth server response.
     request.session['state'] = state
     return Response({"authorization_url": authorization_url})
+
+@api_view(['GET'])
+def GoogleCalendarRedirectView(request):
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = request.session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = REDIRECT_URI
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = request.get_full_path()
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store credentials in the session.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    credentials = flow.credentials
+    request.session['credentials'] = credentials_to_dict(credentials)
+
+
+    return getCalendarList(request=request)
+
+
+def getCalendarList(request):
+    if 'credentials' not in request.session:
+        return request.redirect('google_init')
+
+  # Load credentials from the session.
+    credentials = google.oauth2.credentials.Credentials(**request.session['credentials'])
+    cal_service = googleapiclient.discovery.build(
+      API_SERVICE_NAME, API_VERSION, credentials=credentials)
+      
+    calendar_list = cal_service.calendarList().list().execute()
+
+    # Getting user ID which is his/her email address
+    calendar_id = calendar_list['items'][0]['id']
+    print(calendar_id)
+    # Getting all events associated with a user ID (email address)
+    events  = cal_service.events().list(calendarId=calendar_id).execute()
+
+    events_list_append = []
+    print(events)
+    if not events['items']:
+        print('No data found.')
+        return Response({"message": "No data Found."})
+    else:
+        for events_list in events['items']:
+            events_list_append.append(events_list)
+        return Response({"events": events_list_append})
+
+
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
 
 
 
